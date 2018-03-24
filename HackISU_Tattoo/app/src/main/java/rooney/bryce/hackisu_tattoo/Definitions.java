04dfa1beeadb3168352e2eb56be97345b1747000
awps <awps@iastate.edu>
2018-03-24 17:35:43 -0500

    Merge branch 'master' of https://github.com/renoldhuman/hackisumedicaltattoo
    got all the deg's, mm's, and tags done

--- HackISU_Tattoo/app/src/main/java/rooney/bryce/hackisu_tattoo/Definitions.java
+++ HackISU_Tattoo/app/src/main/java/rooney/bryce/hackisu_tattoo/Definitions.java
@@ -6,11 +6,74 @@ package rooney.bryce.hackisu_tattoo;
 
 
     //Class containing constants that can be accessed from anywhere
+    //Distances are currently in mm!
 public class Definitions {
 
-    public static final int DIABITIES2_DEGREES = 20;
-    public static final int DIABITIES2_DISTANCE = 25;
+    // Endocrine/Immune (Outer)
+
+    public static final int DIABETES1_DEGREES = 45;
+    public static final int DIABETES1_DISTANCE = 27;
+    public static final String DIABETES1_STRING = "Type 1 Diabetes";
+
+    public static final int DIABETES2_DEGREES = 90;
+    public static final int DIABETES2_DISTANCE = 27;
+    public static final String DIABETES2_STRING = "Type 2 Diabetes";
+
+    public static final int HYPOGLYCEMIA_DEGREES = 135;
+    public static final int HYPOGLYCEMIA_DISTANCE = 27;
+    public static final String HYPOGLYCEMIA_STRING = "HYPOGLYCEMIA";
+
+    public static final int HYPOTENSION_DEGREES = 180;
+    public static final int HYPOTENSION_DISTANCE = 27;
+    public static final String HYPOTENSION_STRING = "HYPOTENSION";
+
+    public static final int HYPERGLYCEMIA_DEGREES = 225;
+    public static final int HYPERGLYCEMIA_DISTANCE = 27;
+    public static final String HYPERGLYCEMIA_STRING = "HYPERGLYCEMIA";
+
+    public static final int HYPERTENSION_DEGREES = 260;
+    public static final int HYPERTENSION_DISTANCE = 27;
+    public static final String HYPERTENSION_STRING = "HYPERTENSION";
+
+    public static final int AIDS_DEGREES = 315;
+    public static final int AIDS_DISTANCE = 27;
+    public static final String AIDS_STRING = "AIDS";
 
 
+    // Respiratory/Allergy (Mid)
+
+    public static final int PENICILLIN_DEGREES = 45;
+    public static final int PENICILLIN_DISTANCE = 18;
+    public static final String PENICILLIN_STRING = "Penicillin Allergy";
+
+    public static final int ANTIBIOTICS_DEGREES = 135;
+    public static final int ANTIBIOTICS_DISTANCE = 18;
+    public static final String ANTIBIOTICS_STRING = "Antibiotics Allergy";
+
+    public static final int ASPIRIN_DEGREES = 180;
+    public static final int ASPIRIN_DISTANCE = 18;
+    public static final String ASPIRIN_STRING = "Aspirin Allergy";
+
+    public static final int NSAIDS_DEGREES = 225;
+    public static final int NSAIDS_DISTANCE = 18;
+    public static final String NSAIDS_STRING = "NSAIDS Allergy";
+
+    public static final int FOOD_DEGREES = 315;
+    public static final int FOOD_DISTANCE = 18;
+    public static final String FOOD_STRING = "Food Allergy";
+
+   // Alerts/Misc. (Inner)
+
+    public static final int DNR_DEGREES = 45;
+    public static final int DNR_DISTANCE = 9;
+    public static final String DNR_STRING = "Do Not Resuscitate";
+
+    public static final int NIV_DEGREES = 180;
+    public static final int NIV_DISTANCE = 9;
+    public static final String NIV_STRING = "No IV";
+
+    public static final int DONOR_DEGREES = 315;
+    public static final int DONOR_DISTANCE = 9;
+    public static final String DONOR_STRING = "Donor";
 
 }
