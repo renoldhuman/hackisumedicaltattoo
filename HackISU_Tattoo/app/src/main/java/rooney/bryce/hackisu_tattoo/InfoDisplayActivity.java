d09b8bfaa1cce6d46c260b9fdb364b9a0c0dec19
Brycycle <33557087+Brycycle@users.noreply.github.com>
2018-03-24 15:10:24 -0500

    more structure work

--- HackISU_Tattoo/app/src/main/java/rooney/bryce/hackisu_tattoo/InfoDisplayActivity.java
+++ HackISU_Tattoo/app/src/main/java/rooney/bryce/hackisu_tattoo/InfoDisplayActivity.java
@@ -2,6 +2,10 @@ package rooney.bryce.hackisu_tattoo;
 
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 /**
  * Created by Brycycle on 3/24/2018.
@@ -10,4 +14,34 @@ import android.app.Activity;
 public class InfoDisplayActivity extends Activity {
 
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Intent startMainCameraActivity = new Intent(InfoDisplayActivity.this, MainCameraActivity.class);
+        startActivity(startMainCameraActivity);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+
+    }
 }
--- HackISU_Tattoo/app/src/main/java/rooney/bryce/hackisu_tattoo/MainCameraActivity.java
+++ HackISU_Tattoo/app/src/main/java/rooney/bryce/hackisu_tattoo/MainCameraActivity.java
@@ -1,10 +1,56 @@
 package rooney.bryce.hackisu_tattoo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Brycycle on 3/24/2018.
  */
 
 public class MainCameraActivity extends Activity {
+
+    private Button takePicButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_camera);
+
+        if(savedInstanceState != null){
+
+        }
+
+//        takePicButton = (Button) findViewById(R.id.bTakePic);
+        takePicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Threshhold functioin here. pass info to...
+
+                //Location Data to Readable data function here. Store in intent to send to InfoDisplayActivity
+
+
+                Intent startInfoDisplayActivity = new Intent(MainCameraActivity.this, InfoDisplayActivity.class);
+                //put display info into intent to pass
+                startActivity(startInfoDisplayActivity);
+            }
+        });
+
+
+    }
+
+//    private locationDataPackage thresholdPicFunction(input pic){
+//
+//        return locationDataPackage;
+//    }
+
+//    private infoToDisplayPackage extrapolateInfo(input locationDataPackage){
+//
+//        return infoToDisplayPackage;
+//    }
 }
