192a2bae2accd3cedfb73a33d44af601fb1c6d95
aclucas <aclucas@users.noreply.github.com>
2018-03-24 16:10:29 -0500

    info display
    Added info display

--- HackISU_Tattoo/openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ HackISU_Tattoo/openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Brycycle\\Documents\\GitHub\\hackisumedicaltattoo\\HackISU_Tattoo\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: D:\\Software Development\\hackisumedicaltattoo\\HackISU_Tattoo\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
